7acacd89be6f98b407e6db6126cdfc710654755c
Tyll Weiß <tyll.weiss.extern@axelspringer.com>
2017-09-06 17:54:15 +0200

    TASK: Update to the new code-style guidelines of Immowelt

--- src/commands/default.js
+++ src/commands/default.js
@@ -2,17 +2,17 @@
 
 type ExecFn = (arg: string, isLogging?: boolean) => Promise<void>;
 type Opts = {
-	tags: Array<string>,
-	image: string,
-	arg: string,
-	latest?: string,
-	strip?: string
+  tags: Array<string>,
+  image: string,
+  arg: string,
+  latest?: string,
+  strip?: string
 };
 type BuildOpts = {
-	image: string,
-	arg: string,
-	version: string,
-	tag?: string
+  image: string,
+  arg: string,
+  version: string,
+  tag?: string
 };
 
 const logger = require('log-fancy')('@immowelt/docker-publish');
@@ -22,84 +22,80 @@ const semver = require('semver');
 const asyncExec = require('async-exec');
 
 async function buildAndPush(opts: BuildOpts) {
-	const exec: ExecFn = asyncExec.default;
-	const {
-		image,
-		arg,
-		version,
-		tag = opts.version
-	} = opts;
-	const dockerImageTag = `${image}:${tag}`;
+  const exec: ExecFn = asyncExec.default;
+  const {image, arg, version, tag = opts.version} = opts;
+  const dockerImageTag = `${image}:${tag}`;
 
-	logger.info(`Building ${dockerImageTag}...`);
+  logger.info(`Building ${dockerImageTag}...`);
 
-	try {
-		await exec(`docker build --pull --no-cache --build-arg ${arg}=${version} -t ${dockerImageTag} .`);
-	} catch (e) {
-		logger.warn(`Building ${dockerImageTag} failed with message "${e.message}", continuing to the next published version.`);
-		logger.error(e);
-		return;
-	}
+  try {
+    await exec(
+      `docker build --pull --no-cache --build-arg ${arg}=${version} -t ${dockerImageTag} .`
+    );
+  } catch (e) {
+    logger.warn(
+      `Building ${dockerImageTag} failed with message "${e.message}", continuing to the next published version.`
+    );
+    logger.error(e);
+    return;
+  }
 
-	logger.success(`Successfuly built ${dockerImageTag}!`);
+  logger.success(`Successfuly built ${dockerImageTag}!`);
 
-	logger.info(`Pushing ${dockerImageTag}...`);
-	await exec(`docker push ${dockerImageTag}`, true);
-	logger.success(`Successfuly pushed ${dockerImageTag}!`);
+  logger.info(`Pushing ${dockerImageTag}...`);
+  await exec(`docker push ${dockerImageTag}`, true);
+  logger.success(`Successfuly pushed ${dockerImageTag}!`);
 }
 
 module.exports = async (opts: Opts) => {
-	const {
-		latest = 'latest',
-		strip = false,
-		tags,
-		image,
-		arg
-	} = opts;
-	const url = tags[0];
-	let versionTags = tags;
+  const {latest = 'latest', strip = false, tags, image, arg} = opts;
+  const url = tags[0];
+  let versionTags = tags;
 
-	['image', 'tags', 'arg'].forEach(key => {
-		const value = opts[key];
+  ['image', 'tags', 'arg'].forEach(key => {
+    const value = opts[key];
 
-		if (!value) {
-			throw new Error('Please specify a --tags option to the CLI.');
-		}
-	});
+    if (!value) {
+      throw new Error('Please specify a --tags option to the CLI.');
+    }
+  });
 
-	if (typeof url === 'string' && isUrl(url)) {
-		const res = await fetch(url);
-		const json = await res.json();
+  if (typeof url === 'string' && isUrl(url)) {
+    const res = await fetch(url);
+    const json = await res.json();
 
-		versionTags = json.map(tag => {
-			const str = typeof tag === 'object' ? tag.name : tag;
+    versionTags = json
+      .map(tag => {
+        const str = typeof tag === 'object' ? tag.name : tag;
 
-			return semver.clean(str);
-		}).filter(Boolean);
-	}
+        return semver.clean(str);
+      })
+      .filter(Boolean);
+  }
 
-	//
-	// Build and push each tag.
-	//
-	for (var i = 0; i < versionTags.length; i++) {
-		const version = versionTags[i];
-		const tag = strip && strip.length ? version.replace(strip, '') : version;
+  //
+  // Build and push each tag.
+  //
+  for (var i = 0; i < versionTags.length; i++) {
+    const version = versionTags[i];
+    const tag = strip && strip.length ? version.replace(strip, '') : version;
 
-		await buildAndPush({ // eslint-disable-line no-await-in-loop
-			image,
-			arg,
-			version,
-			tag
-		});
-	}
+    await buildAndPush({
+      // eslint-disable-line no-await-in-loop
+      image,
+      arg,
+      version,
+      tag
+    });
+  }
 
-	//
-	// Renew the generic 'latest' tag of the repository to the latest version of the pwmetrics CLI.
-	//
-	await buildAndPush({
-		image,
-		arg,
-		version: latest,
-		tag: 'latest'
-	});
+  //
+  // Renew the generic 'latest' tag of the repository to the latest version of the pwmetrics CLI.
+  //
+  await buildAndPush({
+    image,
+    arg,
+    version: latest,
+    tag: 'latest'
+  });
 };
--- src/commands/default.spec.js
+++ src/commands/default.spec.js
@@ -4,111 +4,114 @@ const asnycExec = require('async-exec');
 const defaultFlow = require('./default.js');
 
 describe('defaultFlow()', () => {
-	let exec;
+  let exec;
 
-	beforeEach(() => {
-		exec = sinon.stub(asnycExec, 'default').returns(Promise.resolve());
-		nock('http://localhost/')
-			.get('/foo/bar/tags')
-			.reply(200, [{
-				name: 'foo'
-			}, {
-				name: 'v2.0.1'
-			}]);
-	});
+  beforeEach(() => {
+    exec = sinon.stub(asnycExec, 'default').returns(Promise.resolve());
+    nock('http://localhost/')
+      .get('/foo/bar/tags')
+      .reply(200, [
+        {
+          name: 'foo'
+        },
+        {
+          name: 'v2.0.1'
+        }
+      ]);
+  });
 
-	afterEach(() => {
-		exec.restore();
-	});
+  afterEach(() => {
+    exec.restore();
+  });
 
-	it('should be a function.', () => {
-		expect(typeof defaultFlow).toBe('function');
-	});
+  it('should be a function.', () => {
+    expect(typeof defaultFlow).toBe('function');
+  });
 
-	it('should query the given tags URL and build and push the docker image for each of the filtered release tags.', async () => {
-		await defaultFlow({
-			tags: ['http://localhost/foo/bar/tags'],
-			image: 'foo/bar',
-			arg: 'BAR_VERSION'
-		});
+  it('should query the given tags URL and build and push the docker image for each of the filtered release tags.', async () => {
+    await defaultFlow({
+      tags: ['http://localhost/foo/bar/tags'],
+      image: 'foo/bar',
+      arg: 'BAR_VERSION'
+    });
 
-		expect(exec.callCount).toBe(4);
-		expect(exec.args[0][0]).toContain('docker build');
-		expect(exec.args[0][0]).toContain('-t foo/bar:2.0.1');
-		expect(exec.args[0][0]).toContain('--build-arg BAR_VERSION=2.0.1');
+    expect(exec.callCount).toBe(4);
+    expect(exec.args[0][0]).toContain('docker build');
+    expect(exec.args[0][0]).toContain('-t foo/bar:2.0.1');
+    expect(exec.args[0][0]).toContain('--build-arg BAR_VERSION=2.0.1');
 
-		expect(exec.args[1][0]).toContain('docker push');
-		expect(exec.args[1][0]).toContain('foo/bar:2.0.1');
+    expect(exec.args[1][0]).toContain('docker push');
+    expect(exec.args[1][0]).toContain('foo/bar:2.0.1');
 
-		expect(exec.args[2][0]).toContain('docker build');
-		expect(exec.args[2][0]).toContain('-t foo/bar:latest');
-		expect(exec.args[2][0]).toContain('--build-arg BAR_VERSION=latest');
+    expect(exec.args[2][0]).toContain('docker build');
+    expect(exec.args[2][0]).toContain('-t foo/bar:latest');
+    expect(exec.args[2][0]).toContain('--build-arg BAR_VERSION=latest');
 
-		expect(exec.args[3][0]).toContain('docker push');
-		expect(exec.args[3][0]).toContain('foo/bar:latest');
-	});
+    expect(exec.args[3][0]).toContain('docker push');
+    expect(exec.args[3][0]).toContain('foo/bar:latest');
+  });
 
-	it('should allow the usage of raw version tags.', async () => {
-		await defaultFlow({
-			tags: ['2.2.1', '3.0.0'],
-			image: 'foo/bar',
-			arg: 'BAR_VERSION'
-		});
+  it('should allow the usage of raw version tags.', async () => {
+    await defaultFlow({
+      tags: ['2.2.1', '3.0.0'],
+      image: 'foo/bar',
+      arg: 'BAR_VERSION'
+    });
 
-		expect(exec.callCount).toBe(6);
-		expect(exec.args[0][0]).toContain('docker build');
-		expect(exec.args[0][0]).toContain('-t foo/bar:2.2.1');
-		expect(exec.args[0][0]).toContain('--build-arg BAR_VERSION=2.2.1');
+    expect(exec.callCount).toBe(6);
+    expect(exec.args[0][0]).toContain('docker build');
+    expect(exec.args[0][0]).toContain('-t foo/bar:2.2.1');
+    expect(exec.args[0][0]).toContain('--build-arg BAR_VERSION=2.2.1');
 
-		expect(exec.args[1][0]).toContain('docker push');
-		expect(exec.args[1][0]).toContain('foo/bar:2.2.1');
+    expect(exec.args[1][0]).toContain('docker push');
+    expect(exec.args[1][0]).toContain('foo/bar:2.2.1');
 
-		expect(exec.args[2][0]).toContain('docker build');
-		expect(exec.args[2][0]).toContain('-t foo/bar:3.0.0');
-		expect(exec.args[2][0]).toContain('--build-arg BAR_VERSION=3.0.0');
+    expect(exec.args[2][0]).toContain('docker build');
+    expect(exec.args[2][0]).toContain('-t foo/bar:3.0.0');
+    expect(exec.args[2][0]).toContain('--build-arg BAR_VERSION=3.0.0');
 
-		expect(exec.args[3][0]).toContain('docker push');
-		expect(exec.args[3][0]).toContain('foo/bar:3.0.0');
+    expect(exec.args[3][0]).toContain('docker push');
+    expect(exec.args[3][0]).toContain('foo/bar:3.0.0');
 
-		expect(exec.args[4][0]).toContain('docker build');
-		expect(exec.args[4][0]).toContain('-t foo/bar:latest');
-		expect(exec.args[4][0]).toContain('--build-arg BAR_VERSION=latest');
+    expect(exec.args[4][0]).toContain('docker build');
+    expect(exec.args[4][0]).toContain('-t foo/bar:latest');
+    expect(exec.args[4][0]).toContain('--build-arg BAR_VERSION=latest');
 
-		expect(exec.args[5][0]).toContain('docker push');
-		expect(exec.args[5][0]).toContain('foo/bar:latest');
-	});
+    expect(exec.args[5][0]).toContain('docker push');
+    expect(exec.args[5][0]).toContain('foo/bar:latest');
+  });
 
-	it('should allow the usage a custom latest tag.', async () => {
-		await defaultFlow({
-			tags: [],
-			image: 'foo/bar',
-			arg: 'BAR_VERSION',
-			latest: '5.0.1'
-		});
+  it('should allow the usage a custom latest tag.', async () => {
+    await defaultFlow({
+      tags: [],
+      image: 'foo/bar',
+      arg: 'BAR_VERSION',
+      latest: '5.0.1'
+    });
 
-		expect(exec.callCount).toBe(2);
-		expect(exec.args[0][0]).toContain('docker build');
-		expect(exec.args[0][0]).toContain('-t foo/bar:latest');
-		expect(exec.args[0][0]).toContain('--build-arg BAR_VERSION=5.0.1');
+    expect(exec.callCount).toBe(2);
+    expect(exec.args[0][0]).toContain('docker build');
+    expect(exec.args[0][0]).toContain('-t foo/bar:latest');
+    expect(exec.args[0][0]).toContain('--build-arg BAR_VERSION=5.0.1');
 
-		expect(exec.args[1][0]).toContain('docker push');
-		expect(exec.args[1][0]).toContain('foo/bar:latest');
-	});
+    expect(exec.args[1][0]).toContain('docker push');
+    expect(exec.args[1][0]).toContain('foo/bar:latest');
+  });
 
-	it('should allow the stripping of parts from the version tags.', async () => {
-		await defaultFlow({
-			tags: ['2.2.1-alpine', '3.0.0-alpine'],
-			image: 'foo/bar',
-			strip: '-alpine',
-			arg: 'BAR_VERSION'
-		});
+  it('should allow the stripping of parts from the version tags.', async () => {
+    await defaultFlow({
+      tags: ['2.2.1-alpine', '3.0.0-alpine'],
+      image: 'foo/bar',
+      strip: '-alpine',
+      arg: 'BAR_VERSION'
+    });
 
-		expect(exec.args[0][0]).toContain('-t foo/bar:2.2.1');
-		expect(exec.args[0][0]).toContain('--build-arg BAR_VERSION=2.2.1-alpine');
-		expect(exec.args[1][0]).toContain('foo/bar:2.2.1');
+    expect(exec.args[0][0]).toContain('-t foo/bar:2.2.1');
+    expect(exec.args[0][0]).toContain('--build-arg BAR_VERSION=2.2.1-alpine');
+    expect(exec.args[1][0]).toContain('foo/bar:2.2.1');
 
-		expect(exec.args[2][0]).toContain('-t foo/bar:3.0.0');
-		expect(exec.args[2][0]).toContain('--build-arg BAR_VERSION=3.0.0-alpine');
-		expect(exec.args[3][0]).toContain('foo/bar:3.0.0');
-	});
+    expect(exec.args[2][0]).toContain('-t foo/bar:3.0.0');
+    expect(exec.args[2][0]).toContain('--build-arg BAR_VERSION=3.0.0-alpine');
+    expect(exec.args[3][0]).toContain('foo/bar:3.0.0');
+  });
 });
