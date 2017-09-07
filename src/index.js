c43741df38ecd0b893ae943ee72d2abf1c098d6e
Tyll Weiß <tyll.weiss.extern@axelspringer.com>
2017-09-07 14:01:53 +0200

    TASK: Fix the indentation of the main entry point of the CLI

--- src/index.js
+++ src/index.js
@@ -6,33 +6,45 @@ const defaultFlow = require('./commands/default.js');
 const pkg = require('./../package.json');
 
 cli
-	.option('--tags <url | Array<string>>', 'The JSON array to parse or the GitHub API URL which points to a repositories tags.', val => val.split(','))
-	.option('--image <string>', 'The docker image name to use as a base.')
-	.option('--arg <string>', 'The build-arg key which will be used to.')
-	.option('--latest <string>', 'The tag/version to pass into the docker build when building your latest image, defaults to "latest".')
-	.option('--strip <string>', 'An optional string to strip from the generated/fetched docker tags.')
-	.description('Uses/Fetches the given tags, filters out release only tags based on semver and builds/pushes the docker images.')
-	.version(pkg.version)
-	.parse(process.argv);
+  .option(
+    '--tags <url | Array<string>>',
+    'The JSON array to parse or the GitHub API URL which points to a repositories tags.',
+    val => val.split(',')
+  )
+  .option('--image <string>', 'The docker image name to use as a base.')
+  .option('--arg <string>', 'The build-arg key which will be used to.')
+  .option(
+    '--latest <string>',
+    'The tag/version to pass into the docker build when building your latest image, defaults to "latest".'
+  )
+  .option(
+    '--strip <string>',
+    'An optional string to strip from the generated/fetched docker tags.'
+  )
+  .description(
+    'Uses/Fetches the given tags, filters out release only tags based on semver and builds/pushes the docker images.'
+  )
+  .version(pkg.version)
+  .parse(process.argv);
 
 if (!cli.rawArgs.length) {
-	cli.help();
-	process.exit(0);
+  cli.help();
+  process.exit(0);
 }
 
-(async function () {
-	const {tags, image, arg, latest, strip} = cli;
+(async function() {
+  const {tags, image, arg, latest, strip} = cli;
 
-	try {
-		await defaultFlow({
-			latest,
-			tags,
-			image,
-			strip,
-			arg
-		});
-	} catch (err) {
-		logger.error(`Something went wrong while building and pushing the images:`);
-		logger.fatal((err.message || err));
-	}
+  try {
+    await defaultFlow({
+      latest,
+      tags,
+      image,
+      strip,
+      arg
+    });
+  } catch (err) {
+    logger.error(`Something went wrong while building and pushing the images:`);
+    logger.fatal(err.message || err);
+  }
 })();
